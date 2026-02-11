f060f9f805db1dc0e2700e094879fb5e674ca85a
JorgeLG-2406 <jorgegomez242004@gmail.com>
2026-02-11 09:17:16 -0600

    Trabajo del lunes

---
+++ Tecprog/Program.cs
@@ -0,0 +1,17 @@
+﻿// Programa de forma secuencial
+
+Console.WriteLine("Ingresa el Nombre");
+String nombre = Console.ReadLine() ?? "";
+
+Console.WriteLine("Ingresa la Edad");
+int edad = int.Parse( Console.ReadLine() ?? "");
+
+Console.WriteLine($"Nombre: {nombre}");
+Console.WriteLine($"Edad: {edad}");
+
+//Programa Orientado a Objetos
+
+Persona humano1 = new Persona(nombre, edad); //Instancia un nuevo Objeto tipo persona
+
+Console.WriteLine($"Nombre objeto: {humano1. Nombre}");
+Console.WriteLine($"Edad objeto: {humano1. Edad }");
