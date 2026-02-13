653e387e5adb87c2f4f1697a1f3f87b54c3fb1d5
Diegoemigs <159799256+Diegoemigs@users.noreply.github.com>
2026-02-12 23:00:49 -0600

    Update Calculadora.cs

--- Tecprog/Calculadora.cs
+++ Tecprog/Calculadora.cs
@@ -1,8 +1,15 @@
 ﻿//Programa Principal
-Calculadora calculadora = new Calculadora();
-float resultado = calculadora.Division();
+Calculadora calculadora = new Calculadora(10, 5);
+Console.WriteLine("Los números ingresados son:");
+Console.WriteLine("Número 1: " + calculadora.Numero);
+Console.WriteLine("Número 2: " + calculadora.Numero2);
+Console.WriteLine();
 
-Console.WriteLine(resultado);
+// Mostrar resultados de las operaciones
+Console.WriteLine("El resultado de la suma es: " + calculadora.Suma());
+Console.WriteLine("El resultado de la resta es: " + calculadora.Resta());
+Console.WriteLine("El resultado de la multiplicación es: " + calculadora.Multiplicacion());
+Console.WriteLine("El resultado de la división es: " + calculadora.Division());
 
 //Clases
 
@@ -16,8 +23,11 @@ public class Calculadora
     public int Numero2 { get; set; }
 
     //Constructor
-    public Calculadora(int numero, int numero2);
-
+    public Calculadora(int numero, int numero2)
+    {
+        Numero = numero;
+        Numero2 = numero2;
+    }
     //Metodos
     public int Suma() 
     { 
