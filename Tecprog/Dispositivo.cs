6f60ce2d51242d91be853f060140a905718d1613
GonzalezCoronaLeonardo <leonardo.gonzalez.corona@comunidad.unam.mx>
2026-02-25 10:46:35 -0600

    clase miercoles

--- Tecprog/Dispositivo.cs
+++ Tecprog/Dispositivo.cs
@@ -1,28 +1,61 @@
-﻿using System.Reflection;
-using System.Text.RegularExpressions;
+﻿//Programa Principal
+
+Lampara lampara = new Lampara("Lamparita",80,40);
+Ventilador ventilador = new Ventilador("Aire", 500, 50);
+
+//eNCENDER
+
+lampara.Encender();
+ventilador.Encender();
+
+//mOSTRAR INfrormacion
+
+Console.WriteLine(lampara.MostrarInfo());
+Console.WriteLine(ventilador.MostrarInfo());
+
+//aJUSTAR Consumo
+lampara.AjustarConsumo();
+ventilador.AjustarConsumo(150);
+
+//mOSTRAR INfrormacion
+
+Console.WriteLine(lampara.MostrarInfo());
+Console.WriteLine(ventilador.MostrarInfo());
+
+//cOMPARACION
+
+if (lampara > ventilador) {
+    Console.WriteLine("Lampara consume más");
+} else if (lampara < ventilador) {
+    Console.WriteLine("Ventilador concume más");
+} else 
+{
+    Console.WriteLine("Ambos consumen la misma energía");
+}
+
 
 public class Dispositivo 
 {
     //Atributos
     private string nombre;
     private bool encendido;
-    private float consumo;
+    private int consumo;
 
     //Atributos publicos 
     //Atributos Publicos con control
 
-    public virtual string Nombre {
+    public string Nombre {
         get { return nombre; }
         set { nombre = value; }
     }
 
-    public virtual bool Encendido {
-        get { return encendido }
+    public bool Encendido {
+        get { return encendido; }
         set { encendido = value; }
     }
 
-    public float Consumo {
-        get { return consumo; }
+    public int Consumo {
+        get { return encendido ? consumo : 0; }
         set {
             if (value < 0) {
                 consumo = 0;
@@ -33,9 +66,98 @@ public class Dispositivo
     }
     //Constructor
 
-    public Dispositivo(string nombre, bool encendido) 
+    public Dispositivo(string nombre, int consumo) 
     {
         this.nombre = nombre;
-        this.encendido = encendido
+        this.consumo = consumo;
+        this.encendido = false;
+    }
+
+
+    //Metodos
+
+    public void Encender() 
+    {
+        Encendido = true;
+    }
+
+    public void Apagar() 
+    {
+        Encendido = false;
+    }
+
+    //Sobrecarga
+
+    public void AjustarConsumo() 
+    {
+        Consumo = 100;
+    }
+    public void AjustarConsumo(int nuevoConsumo) 
+    {
+        Consumo = nuevoConsumo;
+    }
+
+
+    public virtual string MostrarInfo() 
+    {
+        return $"Dispositivo: {Nombre} \nEncendido: {Encendido} \nConsumo: {Consumo} W";
+    }
+
+    //Sobrecarga Operador
+
+    public static bool operator >(Dispositivo d1, Dispositivo d2) 
+    {
+        return d1.Consumo > d2.Consumo;
+    }
+
+    public static bool operator <(Dispositivo d1, Dispositivo d2)
+    {
+        return d1.Consumo < d2.Consumo;
+    }
+
+    public static bool operator == (Dispositivo d1, Dispositivo d2) 
+    {
+        return d1.Consumo == d2.Consumo;
+    }
+
+    public static bool operator != (Dispositivo d1, Dispositivo d2) {
+        return d1.Consumo != d2.Consumo;
     }
 }
+
+
+
+public class Lampara : Dispositivo {
+    //Atributos
+
+    private int intensidad;//nivel de brillo
+
+    //Constructor
+
+    public Lampara(string nombre, int consumo, int intensidad) : base(nombre, consumo) {
+        this.intensidad = intensidad;
+    }
+    //Metodos
+
+    public override string MostrarInfo() {
+        return $"Lampara: {base.MostrarInfo()} \nIntensidad: {intensidad}";
+    }
+}
+
+public class Ventilador : Dispositivo {
+    //Atributos
+
+    private int velocidad;//nivel de velocidad
+
+    //Constructor
+
+    public Ventilador(string nombre, int consumo, int velocidad) : base(nombre, consumo) {
+        this.velocidad = velocidad;
+    }
+    //Metodos
+
+    public override string MostrarInfo() {
+        return $"Ventilador: {base.MostrarInfo()} \nVelocidad: {velocidad}";
+    }
+}
+    
---
+++ Tecprog/Programaexce.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+
+//Programa Principal
+
+Inventario innventario = new Inventario();
+
+bool salir = false;
+while (!salir) {
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.WriteLine("Menu de suministros");
+    Console.WriteLine("1. Mostrar");
+    Console.WriteLine("2. Buscar");
+    Console.WriteLine("3. Ordenar por Nombre");
+    Console.WriteLine("4. Invertir Orden");
+    Console.WriteLine("5. Vaciar Inventario");
+    Console.WriteLine("6. Agregar Suministro");
+    Console.WriteLine("7. Eliminar Suministro");
+    Console.WriteLine("8. Salir");
+
+    Console.WriteLine("Ingresa tu Selección");
+
+    int opcion = int.Parse(Console.ReadLine() ?? "");
+
+    switch (opcion) { }
+
+}
+
+public class Suministro { 
+    //Atributos
+    public string Nombre { get; set; }
+    public int CantDisp { get; set; }
+    public int Nivel { get; set; }
+
+    public Suministro(string nombre, int cantdisp, int nivel) {
+        Nombre = nombre;
+        CantDisp = cantdisp;
+        Nivel = nivel;
+    }
+
+    public Suministro(string nombreb) {
+        Nombre = nombreb;
+        CantDisp = 1;
+        Nivel = 2;
+    }
+
+    //Metodos
+
+    public void MostrarInfo() {
+        Console.WriteLine($"{Nombre}|Cantidad: {CantDisp}|Nivel: {Nivel}");
+    }
+}
+
+public class Inventario {
+    private Suministro[] suministros;
+
+    //Constructor
+    public Inventario() {
+        suministros = new Suministro[] {
+            new Suministro("Oxigeno",15,1),
+            new Suministro("Gasolina"),
+            new Suministro("Comida",30,1),
+            new Suministro("Almohada",15,3)
+        };
+    }
+} 
